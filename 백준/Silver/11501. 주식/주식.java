dc1e74326b42bfdebe0a920250ceb41e16a13b6f
Yeongbin Kim <133650689+YeongbinKim2000@users.noreply.github.com>
2025-03-08 17:04:13 -0500

    [Silver II] Title: 주식, Time: 1052 ms, Memory: 315644 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,26 +10,21 @@ public class Main {
 
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            int[] arr = new int[n];
+            int[] days = new int[n];
             st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < n; j++) {
-                arr[j] = Integer.parseInt(st.nextToken());
-            }
-
-            int[] maxes = new int[n];
-            maxes[n - 1] = arr[n - 1];
-            int max = arr[n - 1];
-            for (int j = n - 2; j >= 0; j--) {
-                if (arr[j] > max)
-                    max = arr[j];
-
-                maxes[j] = max;
-            }
-            long sum = 0;
             for (int j = 0; j < n; j++)
-                sum += (maxes[j] - arr[j]);
+                days[j] = Integer.parseInt(st.nextToken());
+            long profit = 0;
 
-            System.out.println(sum);
+            int curMax = days[n - 1];
+            for (int j = n - 2; j >= 0; j--) {
+                if (days[j] > curMax)
+                    curMax = days[j];
+
+                profit += (curMax - days[j]);
+            }
+
+            System.out.println(profit);
         }
     }
 }
