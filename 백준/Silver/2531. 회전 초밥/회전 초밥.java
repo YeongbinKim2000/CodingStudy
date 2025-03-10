294a1374562340eb090b58e45f8d5b3626b291b0
Yeongbin Kim <133650689+YeongbinKim2000@users.noreply.github.com>
2025-03-09 22:57:46 -0400

    [Silver I] Title: 회전 초밥, Time: 168 ms, Memory: 18616 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,55 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        int n = Integer.parseInt(st.nextToken()); // 초밥 접시 수
-        int d = Integer.parseInt(st.nextToken()); // 초밥 가짓 수
-        int k = Integer.parseInt(st.nextToken()); // 연속 접시 수
-        int c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
-
+        
+        int n = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        int max = 0;
         int[] belt = new int[n];
-        for (int i = 0; i < belt.length; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int cur = Integer.parseInt(st.nextToken());
-            belt[i] = cur;
+        for (int i = 0; i < n; i++) {
+            belt[i] = Integer.parseInt(br.readLine());
         }
-
-        int[] sushiCount = new int[d + 1];
-        int uniqueSushi = 0;
-
+        int[] sushi = new int[d + 1];
+        int curCnt = 0;
         for (int i = 0; i < k; i++) {
-            if (sushiCount[belt[i]] == 0)
-                uniqueSushi++;
-            sushiCount[belt[i]]++;
+            int curSushi = belt[i];
+            if (sushi[curSushi] == 0)
+                curCnt++;
+            sushi[curSushi]++;
         }
-
-        int max = uniqueSushi;
-        if (sushiCount[c] == 0)
-            max++;
-
-        for (int i = 1; i < n; i++) {
-            int removeSushi = belt[i - 1];
-            sushiCount[removeSushi]--;
-            if (sushiCount[removeSushi] == 0)
-                uniqueSushi--;
-
-            int addSushi = belt[(i + k - 1) % n];
-            if (sushiCount[addSushi] == 0)
-                uniqueSushi++;
-            sushiCount[addSushi]++;
-
-            if (sushiCount[c] == 0)
-                max = Math.max(max, uniqueSushi + 1);
+        if (sushi[c] == 0)
+            max = Math.max(max, curCnt + 1);
+        else
+            max = Math.max(max, curCnt);
+        
+        for (int i = 0; i < n; i++) {
+            sushi[belt[i]]--;
+            if (sushi[belt[i]] == 0)
+                curCnt--;
+            int nextIdx = (i + k) % n;
+            if (sushi[belt[nextIdx]] == 0)
+                curCnt++;
+            sushi[belt[nextIdx]]++;
+            
+            if (sushi[c] == 0)
+                max = Math.max(max, curCnt + 1);
             else
-                max = Math.max(max, uniqueSushi);
+                max = Math.max(max, curCnt);
         }
-
+        
         System.out.println(max);
     }
 }
