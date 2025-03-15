903b57607d6694c25cd98bba306b6a2fe08b97d0
Yeongbin Kim <133650689+YeongbinKim2000@users.noreply.github.com>
2025-03-15 16:12:45 -0400

    [Gold V] Title: 암호 만들기, Time: 176 ms, Memory: 18744 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,58 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static HashSet<String> passwords;
     static int l;
+    static int c;
+    static char[] arr;
+    static ArrayList<String> possible;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         l = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-
+        c = Integer.parseInt(st.nextToken());
+        arr = new char[c];
+        possible = new ArrayList<>();
         st = new StringTokenizer(br.readLine(), " ");
-        char[] characters = new char[c];
-        for (int i = 0; i < characters.length; i++) {
-            characters[i] = st.nextToken().charAt(0);
+        for (int i = 0; i < c; i++)
+            arr[i] = st.nextToken().charAt(0);
+        Arrays.sort(arr);
+        boolean[] visited = new boolean[c];
+        create("", 0, 0, 0, visited);
+
+        Collections.sort(possible);
+        for (int i = 0; i < possible.size(); i++) {
+            System.out.println(possible.get(i));
         }
-
-        ArrayList<Character> vowels = new ArrayList<>();
-        vowels.add('a');
-        vowels.add('e');
-        vowels.add('i');
-        vowels.add('o');
-        vowels.add('u');
-
-
-        Arrays.sort(characters);
-
-        passwords = new HashSet<>();
-
-        makePasswords(characters, vowels, "", 0, 0, 0);
-
-        List<String> tempSet = new ArrayList<>(passwords);
-        Collections.sort(tempSet);
-
-        for (int i = 0; i < tempSet.size(); i++)
-            System.out.println(tempSet.get(i));
     }
 
-    private static void makePasswords(char[] characters, ArrayList<Character> vowels, String cur, int idx, int vowelNum, int consonantNum) {
-        if (cur.length() == l) {
-            if (vowelNum >= 1 && consonantNum >= 2)
-                passwords.add(cur);
+    private static void create(String cur, int idx, int cnt1, int cnt2, boolean[] visited) {
+        if (cur.length() == l && cnt1 >= 1 && cnt2 >= 2) {
+            possible.add(cur);
             return;
         }
 
-        for (int i = idx; i < characters.length; i++) {
-            if (vowels.contains(characters[i]))
-                makePasswords(characters, vowels, cur + characters[i], i + 1, vowelNum + 1, consonantNum);
-            else
-                makePasswords(characters, vowels, cur + characters[i], i + 1, vowelNum, consonantNum + 1);
-//            makePasswords(characters, vowels, cur, i + 1, vowelNum, consonantNum);
+        for (int i = idx; i < visited.length; i++) {
+            if (!visited[i]) {
+                int newCnt1 = cnt1;
+                int newCnt2 = cnt2;
+                if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u')
+                    newCnt1++;
+                else
+                    newCnt2++;
+                String temp = cur + arr[i];
+                visited[i] = true;
+                create(temp, i + 1, newCnt1, newCnt2, visited);
+                visited[i] = false;
+            }
         }
     }
 }
