b12e0a8c10d8728cfa9300e7dae263022f29121a
Yeongbin Kim <133650689+YeongbinKim2000@users.noreply.github.com>
2025-03-20 21:49:20 -0400

    [Gold V] Title: 최소비용 구하기, Time: 500 ms, Memory: 51140 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -2,20 +2,22 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static ArrayList<Node>[] graph;
-    static boolean[] visited;
+    static int n;
+    static int m;
+    static ArrayList<Node1>[] graph;
     static int[] dist;
+    static boolean[] visited;
     static int end;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
         graph = new ArrayList[n + 1];
         dist = new int[n + 1];
         visited = new boolean[n + 1];
-
         for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
             dist[i] = Integer.MAX_VALUE;
@@ -23,11 +25,11 @@ public class Main {
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            graph[from].add(new Node(to, cost));
+            graph[a].add(new Node1(b, c));
         }
 
         st = new StringTokenizer(br.readLine(), " ");
@@ -39,43 +41,44 @@ public class Main {
         System.out.println(dist[end]);
     }
 
-    private static void dijkstra(int cur) {
-        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
+    private static void dijkstra(int start) {
+        PriorityQueue<Node1> pq = new PriorityQueue<>(new Comparator<Node1>(){
+            public int compare(Node1 o1, Node1 o2) {
                 return o1.cost - o2.cost;
             }
         });
 
-        pq.add(new Node(cur, 0));
-        dist[cur] = 0;
+        pq.add(new Node1(start, 0));
+        dist[start] = 0;
 
         while (!pq.isEmpty()) {
-            Node now = pq.poll();
+            Node1 curNode = pq.poll();
+            int curValue = curNode.v;
 
-            if (!visited[now.v])
-                visited[now.v] = true;
+            if (!visited[curValue])
+                visited[curValue] = true;
             else
                 continue;
 
-            for (int i = 0; i < graph[now.v].size(); i++) {
-                Node next = graph[now.v].get(i);
+            for (int i = 0; i < graph[curValue].size(); i++) {
+                Node1 nextNode = graph[curValue].get(i);
 
-                if (!visited[next.v] && dist[next.v] > now.cost + next.cost) {
-                    dist[next.v] = now.cost + next.cost;
-                    pq.add(new Node(next.v, dist[next.v]));
+                if (!visited[nextNode.v] && dist[nextNode.v] > curNode.cost + nextNode.cost) {
+                    dist[nextNode.v] = curNode.cost + nextNode.cost;
+                    pq.add(new Node1(nextNode.v, dist[nextNode.v]));
                 }
             }
         }
     }
 
-    private static class Node {
+    private static class Node1 {
         int v;
         int cost;
 
-        private Node(int v, int cost) {
+        private Node1 (int v, int cost) {
             this.v = v;
             this.cost = cost;
         }
     }
 }
+
