57cfdf636f9a955dba8db650ae72fce337098216
Yeongbin Kim <133650689+YeongbinKim2000@users.noreply.github.com>
2026-05-03 17:31:48 +0900

    [D5] Title: [S/W 문제해결 응용] 6일차 - K번째 접미어, Time: 258 ms, Memory: 120,336 KB -BaekjoonHub

---
+++ SWEA/D5/1256. ［S／W 문제해결 응용］ 6일차 － K번째 접미어/［S／W 문제해결 응용］ 6일차 － K번째 접미어.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, key, data=None):
+        self.key = key
+        self.data = data
+        self.children = {}
+
+
+def insert(root, word):
+    cur = root
+
+    for ch in word:
+        if ch not in cur.children:
+            cur.children[ch] = Node(ch)
+        cur = cur.children[ch]
+
+    cur.data = word
+
+
+def dfs(node):
+    global cnt, answer, k
+
+    if node.data is not None:
+        cnt += 1
+        if cnt == k:
+            answer = node.data
+            return
+
+    for ch in sorted(node.children.keys()):
+        dfs(node.children[ch])
+        if answer:
+            return
+
+
+tc = int(input())
+
+for t in range(1, tc + 1):
+    k = int(input())
+    word = input()
+
+    root = Node(None)
+
+    for i in range(len(word)):
+        insert(root, word[i:])
+
+    cnt = 0
+    answer = ""
+
+    dfs(root)
+
+    if answer:
+        print(f"#{t} {answer}")
+    else:
+        print(f"#{t} none")
